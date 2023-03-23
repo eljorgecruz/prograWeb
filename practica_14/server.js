a6b6121f6d6babef04a8cba2d98d286cb6ee1c19
Jorge Cruz <jcruz42@ucol.mx>
2023-03-23 11:46:21 -0600

    Correccion de css

--- practica_14/server.js
+++ practica_14/server.js
@@ -16,6 +16,7 @@ app.get('/', function (req, res) {
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="/assets/style.css" type="text/css" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
