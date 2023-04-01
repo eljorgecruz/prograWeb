db544db0193cdf04a2dc22f9b735e8a790284d97
Jorge Cruz <jcruz42@ucol.mx>
2023-03-31 21:45:12 -0600

    Aplicacion de css

--- practica_17/src/routes/person.js
+++ practica_17/src/routes/person.js
@@ -14,20 +14,13 @@ router.post('/addStudent', function (req, res) {
     });
 });
 
-// usamos el expresion "exprese.json" para parsear el body de la peticion y poder
-// acceder a los datos del objeto pero tranformdo a string
-router.post('/personJson', express.json({type:'*/*'}), (req, res) => {
+// usamos el expresion "express.json" para parsear el body de la peticion y poder
+// acceder a los datos del objeto en nuestro archivo ejs pero tranformdo a string
+router.post('/personJson', express.json({type: '*/*'}), (req, res) => {
     // el objeto lo recibe del archivo testJson.ejs ya que este es un archvo json
     // y se envia a traves de un formulario del body
-    let t= req.params.id;
-    res.render('testJson');
-    // console.log(`nombre: ${req.body.nombre}
-    //              apellido: ${req.body.apellido} `)
-    res.render('personJson',
-        {
-            nombre: req.body.fname,
-            apellido: req.body.lname
-        });
+    console.log(`nombre: ${req.body.nombre},
+                 apellido: ${req.body.apellido}`);
 });
 
 router.get('/testJson', function(req, res){
